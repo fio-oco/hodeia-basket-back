25e2b54a0cadbb0a2f766ebdcb8886fb7db93785
fio-oco <fiona-oconnell@hotmail.com>
2023-12-11 19:54:42 +0100

    more entities

--- src/teams/team.entity.ts
+++ src/teams/team.entity.ts
@@ -1,7 +1,8 @@
 import { Liga } from 'src/ligas/liga.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, ManyToOne} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, ManyToOne, OneToMany} from 'typeorm';
 import { v4 as uuid } from 'uuid';
+import { Match } from 'src/matches/match.entity';
 
 @Entity({name: 'equipos'}) // getting errors here (I think I need it but it breaks the database connection when I added it but this way blank before)
 export class Team {
@@ -18,14 +19,23 @@ export class Team {
   entrenadorid: User; //to bring the obj the the user
 
   // each league has many teams, each team can only have one league (many to one here)
- /*  @ManyToOne(() => Liga, liga => liga.teams)
+  @ManyToOne(() => Liga, liga => liga.teams)
   @Column({type: 'uuid'})
-  @JoinColumn({name:ligaid})
-  ligaid: uuid;
- */
+  @JoinColumn({name: 'ligaid'})
+  liga: uuid;
+
   @Column({type: 'varchar'})
   genero: string;
 
   @Column({type: 'varchar'})
   equipoLogo: string;
+
+  @Column({type: 'varchar'})
+  ciudad: string;
+
+  @OneToMany(()=> Match, match => match.visitanteid)
+  matches: Match[];
+
+ /*  @OneToMany(()=> Match, match => match.localid)
+  matches: Match[]; */ //getting an error here can't set duplicate values for matches, have to distinguish between them maybe matches as local??
 } 
