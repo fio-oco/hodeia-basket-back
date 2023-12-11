f5b2ec02bdf522185501a79a7ad24d4fef8494e1
fio-oco <fiona-oconnell@hotmail.com>
2023-12-11 16:01:28 +0100

    edit to user.entity re gender

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, PrimaryGeneratedColumn, BeforeInsert, ManyToMany, JoinTable, ManyToOne, JoinColumn, OneToOne } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, BeforeInsert, ManyToOne, JoinColumn, OneToOne } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 import { Role } from './role.entity';
+import { Team } from 'src/teams/team.entity';
 
 @Entity({name: 'usuarios'})
 export class User {
@@ -18,14 +19,13 @@ export class User {
   @Column({ type: 'varchar', length: 100 })
   email: string;
 
+  @Column({type: 'varchar'})
+  gender: string;
+
   @ManyToOne(() => Role, role => role.users) 
   @Column({ type: 'uuid', nullable: false, default: 'e144cde5-91df-4f7e-b808-c984ba493507'})
   @JoinColumn({ name: 'rol'}) //{ name: 'rol' } specifies that the rolData property in the User entity is the column in the User table that should be used to join with the Role entity.
   rol: Role; //relationship in the User entity. It is of type Role, indicating that it holds an instance of the Role entity associated with a specific user.
-
-  // @Column({name: 'rolid', type: 'uuid', nullable: false, default: 'e144cde5-91df-4f7e-b808-c984ba493507'}) 
-  // rolId: uuid; //this has to match the column in the usuarios table
-// to remember: don't have problems setting a particular rol when I create a user, but when I try the patch to update rol I get an error stating that rol doesn't exist on user.. even though it is here, will figure it out tomorrow.
 
   @Column({ type: 'varchar'})
   password: string;
@@ -43,7 +43,8 @@ export class User {
   @Column({type: 'boolean', default: true})
   isActive: boolean;
 
-  //@OneToOne(())
+  @OneToOne(() => Team, team => team.entrenadorid)
+  team: Team;
 }
 
 /*   @Column({ type: 'enum', enum: ['m', 'f', 'u'] })
