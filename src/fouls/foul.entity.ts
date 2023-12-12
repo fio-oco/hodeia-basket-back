6e5688377a15a3ea6dd9bc4c7afad6987b9a152b
fio-oco <fiona-oconnell@hotmail.com>
2023-12-12 18:27:55 +0100

    backup and entity updates

---
+++ src/fouls/foul.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, OneToMany, Timestamp, ManyToOne, JoinColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Player } from 'src/players/player.entity';
+import { Match } from 'src/matches/match.entity';
+
+@Entity({name: 'faltas_partido'}) // getting errors here (I think I need it but it breaks the database connection when I added it but this way blank before)
+export class Foul {
+  @PrimaryGeneratedColumn('uuid')
+  faltaid: string;
+
+  @ManyToOne(() => Player, player => player.player_fouls)
+  @JoinColumn({name: 'jugadorid'})
+  @Column({ type: 'uuid'})
+  jugadorid: uuid;
+
+  //relation here
+  @Column({type: 'uuid'})
+  partidoid: uuid;
+
+  @Column({type: 'timestamp'})
+  marcatiempo: Date;
+
+}
---
+++ src/scores/score.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, JoinColumn, ManyToOne} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Match } from 'src/matches/match.entity';
+import { Player } from 'src/players/player.entity';
+
+
+@Entity({name: 'puntos_partido'}) // getting errors here (I think I need it but it breaks the database connection when I added it but this way blank before)
+export class Score {
+  @PrimaryGeneratedColumn('uuid')
+  puntoid: uuid;
+ 
+  @ManyToOne(() => Player, player => player.player_scores)
+  @JoinColumn({name: 'jugadorid'})
+  @Column({ type: 'uuid'})
+  jugadorid: uuid;
+
+ @ManyToOne(() => Match, match => match.match_scores)
+ @JoinColumn({name: 'partidoid'})
+  @Column({type: 'uuid'})
+  partidoid: uuid;
+
+  @Column({type: 'timestamp'})
+  marcatiempo: Date;
+
+  @Column({type: 'integer'})
+  puntos: number;
+
+} 
