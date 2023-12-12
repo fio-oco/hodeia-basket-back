8ecccc812859bb2ecc597d814122bf62680b5c47
fio-oco <fiona-oconnell@hotmail.com>
2023-12-12 13:58:28 +0100

    team gets

---
+++ src/teams/team.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TeamService } from './team.service';
+import { TeamController } from './team.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Team } from './team.entity';
+import { Match } from 'src/matches/match.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Liga } from 'src/ligas/liga.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Team, Match, User, Liga])],
+  controllers: [TeamController],
+  providers: [TeamService]
+})
+export class TeamModule {}
