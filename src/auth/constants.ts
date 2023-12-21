3b60ba29686ce490c771d3141fcdc81bbb5580a5
fio-oco <fiona-oconnell@hotmail.com>
2023-12-21 20:54:16 +0100

    git.ignore

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,6 +1,3 @@
 export const jwtConstants = {
     secret: 'pongamosloquesea1234321'
 }
-
-//need to ask Peio about this ^^
-//this code would be in an .env file which then goes into your GIT-ignore so that it isn't shared when you upload your application 
